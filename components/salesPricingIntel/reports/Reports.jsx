d90085f82ef1a4b4fc9c8450603ddd164b6464b1
Rohit bhai rajput <rohit.rajput@mindnerves.com>
2023-03-06 20:20:34 +0530

    Rewamped

--- components/salesPricingIntel/reports/Reports.jsx
+++ components/salesPricingIntel/reports/Reports.jsx
@@ -1,8 +1,8 @@
+import { Switch } from "antd";
 import React from "react";
 import NSCard from "../../common/NSCard";
-import ReportsRowOne from "./ReportsRowOne";
-import ReportsRowTwo from "./ReportsRowTwo";
-import RepricingHighlightsTileOne from "./RepricingHighlightsTileOne";
+import NSDropdown from "../../common/NSDropdown";
+import NSInput from "../../common/NSInput";
 import RepricingHighlightsTileTwo from "./RepricingHighlightsTileTwo";
 
 const Performance = () => {
@@ -13,27 +13,101 @@ const Performance = () => {
       </h1>
 
       <div>
-        <NSCard className="my-3 p-4">
-          <ReportsRowOne />
-          <ReportsRowTwo />
+        <NSCard className="my-3 p-4 grid grid-cols-3">
+          <div className="col-span-1 border-r-2 p-2">
+            <h1 className="font-MontBold mb-2">Revenue boosted</h1>
+            <div className="flex">
+              <p className="w-[50%]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus, quo!</p>
+            </div>
+          </div>
+          <div className="col-span-1 border-r-2 p-2">
+            <h1 className="font-MontBold mb-2">Price Optimized Products</h1>
+            <div className="flex">
+              <p className="w-[50%]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus, quo!</p>
+            </div>
+          </div>
+          <div className="col-span-1 p-2">
+            <h1 className="font-MontBold mb-2">Products. at Optim Lim</h1>
+            <div className="flex">
+              <p className="w-[50%]">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus, quo!</p>
+            </div>
+          </div>
+        </NSCard>
+        <NSCard className="my-3 p-4 grid grid-cols-6 gap-3">
+          <div className="col-span-2  border rounded-md">
+            <div className="flex">
+              <div className="repricing-intervals h-[100%] w-[100%] p-2 mb-2 mr-2">
+                <h2 className="font-bold">Repricing intervals</h2>
+                <div className="flex items-center justify-between">
+                  <p className="text-[0.8rem]">New price should be</p>
+                  <NSDropdown options={[]} className="w-[110px]" />
+                </div>
+                <div className="h-20 bg-[var(--bg-main)] mt-2"></div>
+                <p className="text-blue-600 text-[.7rem] font-bold mt-2">
+                  Current Price - 5% and Current Price + 5%
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-3 border border-gray-300 rounded-md p-2">
+            <div className="flex justify-between mr-2">
+              <div className="new-pricingp-2 w-[100%]">
+                <h2 className="font-bold truncate">
+                  New Price should be higher than
+                </h2>
+                <div className="flex justify-between mt-3">
+                  <NSDropdown options={[]} className="w-[200px]" />
+                  <NSDropdown options={[]} className="w-[200px]" />
+                  <NSInput
+                    type="text"
+                    className="w-[100px]"
+                    style={{ height: "10px", width: "100px" }}
+                  />
+                </div>
+                <h2 className="font-bold truncate mt-2">
+                  New Price should be lower than
+                </h2>
+                <div className="flex justify-between mt-3">
+                  <NSDropdown options={[]} className="w-[200px] mr-[.22px]" />
+                  <NSDropdown options={[]} className="w-[200px] mr-[.22px]" />
+                  <NSInput
+                    type="text"
+                    className="w-[100px]"
+                    style={{ height: "10px", width: "100px" }}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-1 border border-gray-300 rounded-md">
+            <div className="max-items-amount new-pricing p-2">
+              <Switch />
+              <h2 className="text-[.8rem] font-bold mt-2">
+                Maximum items amount for pricing
+              </h2>
+              <NSInput
+                type="text"
+                style={{ height: "30px" }}
+                className="mt-2"
+              />
+            </div>
+          </div>
         </NSCard>
 
-        <div>
-          <h1 className="text-[1.4rem] font-bold mb-3">
-            Your Persona Selection Settings
-          </h1>
-          <div className="grid grid-cols-3 gap-2">
-            <RepricingHighlightsTileOne />
+        <div className="grid grid-cols-2 gap-3">
+          <NSCard className="col-span-1">
             <RepricingHighlightsTileTwo
               title="Our Price"
               subtitle="INR 4,232"
               showupDownArrow={false}
             />
+          </NSCard>
+          <NSCard className="col-span-1">
             <RepricingHighlightsTileTwo
               title="SKU in Focus"
               showupDownArrow={false}
             />
-          </div>
+          </NSCard>
         </div>
       </div>
     </div>
