88746d7daa6bd0ee705bd855b30777ab8e8ffbb3
jitendra11-code <jpkamal234@gmail.com>
2023-06-07 13:41:07 +0530

    card border radius

--- components/common/NSCard.jsx
+++ components/common/NSCard.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function NSCard({ children, className, style }) {
   return (
     <div
-      className={`${className} bg-white p-2 shadow-[var(--light-shadow)] rounded-md h-auto`}
+      className={`${className} bg-white p-2 shadow-[var(--light-shadow)] rounded-[15px] h-auto`}
       style={style}
     >
       {children}
